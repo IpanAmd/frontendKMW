58c2f3924bd7b0ff89c4c9fe9a93de45a51e82f0
IpanAmd <ipanamanda@gmail.com>
2023-01-19 17:19:04 +0700

    menampilkan hasil perhitungan cf

--- src/pages/HasilDiagnosa.jsx
+++ src/pages/HasilDiagnosa.jsx
@@ -15,10 +15,11 @@ function HasilDiagnosa() {
 
   const [consultations, setConsultations] = useState([]);
   const [results, setResults] = useState([]);
+  const [kb, setKb] = useState([]);
 
   const clientId = localStorage.getItem("client");
   const navigate = useNavigate();
-
+  // mendapatkan data client berdasarkan id
   const dataClient = () => {
     axios
       .get(`http://localhost:8000/api/v1/client/${clientId}`)
@@ -31,25 +32,58 @@ function HasilDiagnosa() {
       });
   };
 
-  const dataGejala = [];
+  // mendapatkan semua data konsultasi
   const dataKonsultasi = () => {
     axios.get(`http://localhost:8000/api/v1/consultation`).then((response) => {
       setConsultations(response.data.data);
     });
   };
-  consultations.sort((a, b) => a.id - b.id);
-  consultations.map((data) => {
-    if (data.Client.id == clientId) {
-      dataGejala.push(data.Indication);
-    }
-  });
-  console.log(dataGejala, "gejala");
 
+  // mendapatkan semua data hasil kerusakan
   const dataHasil = () => {
     axios.get(`http://localhost:8000/api/v1/result`).then((response) => {
       setResults(response.data.data);
     });
   };
+
+  // mendapatkan semua data basis pengetahuan
+  const dataBasisPengetahuan = () => {
+    axios
+      .get(`http://localhost:8000/api/v1/knowledge-base`)
+      .then((response) => {
+        setKb(response.data.data);
+      });
+  };
+
+  // menampung data gejala dari konsultasi berdasarkan client id
+  const dataGejala = [];
+  // menampung data eviden berdasarkan gejala yg dipilih
+  const dataEvidence = [];
+
+  consultations.sort((a, b) => a.id - b.id);
+  consultations.map((data) => {
+    if (data.Client.id == clientId) {
+      let obj = {
+        cf_user: data.cf_user,
+        indication: data.Indication.name,
+      };
+      dataGejala.push(obj);
+      results.map((result) => {
+        if (data.Client.id == clientId) {
+          if (result.Client.id == clientId) {
+            let obj = {
+              evidence: data.cf_user,
+              indication: data.Indication.name,
+              fault: result.Fault.name,
+            };
+            dataEvidence.push(obj);
+          }
+        }
+      });
+    }
+  });
+
+  // mencari data kerusakan berdarkan hasil konsultasi
   const dataKerusakan = [];
   results.sort((a, b) => a.id - b.id);
   results.map((data) => {
@@ -57,9 +91,68 @@ function HasilDiagnosa() {
       dataKerusakan.push(data.Fault);
     }
   });
-  console.log(dataKerusakan, "kerusakan");
+  console.log(dataGejala, "gejala");
 
-  const tombolKembali = () => {
+  // mencari data hipotesis berdasarkan basis pengetahuan
+  const dataHipotesis = [];
+  kb.sort((a, b) => a.id - b.id);
+  kb.map((data) => {
+    dataKerusakan.map((fault) => {
+      dataGejala.map((indication) => {
+        if (
+          data.Fault.name == fault.name &&
+          data.Indication.name == indication.indication
+        ) {
+          let obj = {
+            hipotesis: data.mb,
+            indication: data.Indication.name,
+            fault: data.Fault.name,
+          };
+          dataHipotesis.push(obj);
+        }
+      });
+    });
+  });
+  console.log(dataHipotesis, "H");
+  console.log(dataEvidence, "E");
+
+  // mencari nilai setiap cf tunggal
+  const cfTunggal = [];
+  dataHipotesis.map((h) => {
+    dataEvidence.map((e) => {
+      if (h.fault == e.fault && h.indication == e.indication) {
+        let obj = {};
+        let value = Math.round(h.hipotesis * e.evidence * 100) / 100;
+        obj["cf"] = value;
+        obj["fault"] = h.fault;
+        cfTunggal.push(obj);
+      }
+    });
+  });
+  console.log(cfTunggal, "cf");
+
+  // mendapatkan nilai cf kombinasi
+  const hasilAkhir = [];
+  let result = 0;
+  dataKerusakan.map((fault) => {
+    let obj = {};
+    let value = 0;
+    cfTunggal.map((data) => {
+      if (data.fault == fault.name) {
+        value = result + data.cf * (1 - result);
+        result = value;
+      }
+    });
+    obj["cf"] = (Math.round(result * 100) / 100) * 100;
+    obj["fault"] = fault.name;
+    obj["solution"] = fault.solution;
+    hasilAkhir.push(obj);
+    result = 0;
+  });
+  console.log(hasilAkhir, "cekNilai");
+
+  // aksi untuk tombol selesai
+  const tombolSelesai = () => {
     localStorage.removeItem("client");
     localStorage.removeItem("consultation");
     navigate("/");
@@ -69,6 +162,7 @@ function HasilDiagnosa() {
     dataClient();
     dataKonsultasi();
     dataHasil();
+    dataBasisPengetahuan();
   }, []);
   return (
     <>
@@ -116,37 +210,86 @@ function HasilDiagnosa() {
               </table>
             </div>
             <div id="Gejala">
-              <p>Gejala Kerusakan</p>
-              <ol>
-                {dataGejala.map((data) => {
-                  return <li key={data.id}>&nbsp;{data.name}</li>;
-                })}
-              </ol>
-            </div>
-            <div id="kerusakan">
               <div className="d-flex justify-content-between">
-                <p>Hasil Diagnosa</p>
-                <p>Nilai Kepastian</p>
+                <p>Gejala Kerusakan</p>
+                <p>Tingkat Kepercayaan</p>
               </div>
               <div className="d-flex flex-row justify-content-between">
                 <div>
                   <ol>
-                    {dataKerusakan.map((data) => {
-                      return <li key={data.id}>&nbsp;{data.name}</li>;
-                    })}
+                    {
+                      (dataGejala.sort((a, b) => b.evidence - a.evidence),
+                      dataGejala.map((data, index) => {
+                        return <li key={index}>{data.indication}</li>;
+                      }))
+                    }
                   </ol>
                 </div>
+                <div className={style.sizeDiv}>
+                  {
+                    (dataGejala.sort((a, b) => b.evidence - a.evidence),
+                    dataGejala.map((data, index) => {
+                      let value = "";
+                      if (data.cf_user == 0) {
+                        value = "Tidak Yakin";
+                      } else if (data.cf_user == 0.2) {
+                        value = "Sedikit Yakin";
+                      } else if (data.cf_user == 0.4) {
+                        value = "Cukup Yakin";
+                      } else if (data.cf_user == 0.6) {
+                        value = "Yakin";
+                      } else {
+                        value = "Sangat Yakin";
+                      }
+                      return (
+                        <label className="m-0" key={index}>
+                          {value}
+                        </label>
+                      );
+                    }))
+                  }
+                </div>
+              </div>
+            </div>
+            <div id="kerusakan">
+              <div className="d-flex justify-content-between">
+                <p>Hasil Diagnosa</p>
+                <p className="me-4">Nilai Kepastian</p>
+              </div>
+              <div className="d-flex flex-row justify-content-between">
                 <div>
-                  <label>%</label>
+                  <ol>
+                    {
+                      (hasilAkhir.sort((a, b) => b.cf - a.cf),
+                      hasilAkhir.map((data, index) => {
+                        return <li key={index}>{data.fault}</li>;
+                      }))
+                    }
+                  </ol>
+                </div>
+                <div className={style.sizeDiv}>
+                  {
+                    (hasilAkhir.sort((a, b) => b.cf - a.cf),
+                    hasilAkhir.map((data, index) => {
+                      return (
+                        <label className={style.listNone} key={index}>
+                          &nbsp;{data.cf}%
+                        </label>
+                      );
+                    }))
+                  }
                 </div>
               </div>
             </div>
             <div id="solusi">
               <p>Solusi</p>
               <ol>
-                {dataKerusakan.map((data) => {
-                  return <li key={data.id}>&nbsp;{data.solution}</li>;
-                })}
+                {
+                  (hasilAkhir.sort((a, b) => b.cf - a.cf),
+                  hasilAkhir.map((data, index) => {
+                    return <li key={index}>{data.solution}</li>;
+                  }))
+                }
               </ol>
             </div>
           </main>
@@ -155,9 +298,9 @@ function HasilDiagnosa() {
               variant="danger"
               className="mx-3"
               type="submit"
-              onClick={tombolKembali}
+              onClick={tombolSelesai}
             >
-              Kembali
+              Selesai
             </Button>
           </div>
         </Container>
